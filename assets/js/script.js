c62f462e01e9dcde0a19313102ce1a4c38f88e15
caroll-tech <carolitatapia@gmail.com>
2024-02-17 20:31:26 -0300

    se agregan tooltip en navbar

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+});
+
+$("#enviar").click(function (e) {
+  alert("Sus comentarios se han enviado correctamente");
+});
